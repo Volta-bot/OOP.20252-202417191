42a0e351e4d843b78f21fcc3e4f4d5de0d765175
Ngoc Son <sondoannguyenngoc@gmail.com>
2026-03-05 15:56:51 +0700

    Assign 6.3 and 6.4

---
+++ Homework02/Assignment6_3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Assignment6_3 {
+	public static void main(String[] args) {
+		Scanner Input = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int n = Input.nextInt();
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=(n-i);j++) {
+				System.out.print(" ");
+			}
+			for(int j=1; j<=2*i-1;j++) {
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+	}
+}
